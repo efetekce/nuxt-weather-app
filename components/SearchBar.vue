b3fc24e981ea6c3e0d0d3527b81523464c9e08f9
Efe <efete_kce@proton.me>
2024-04-13 12:52:52 +0300

    pinia store added, api testing

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -7,13 +7,19 @@ const searchTerm = reactive({
 });
 
 const apiKey = "f6038edf8d2a4a7c89701417241104";
+const apiKey2 = "47hAIuP8o8M9RSBosP20WsurcfPSgbAu";
 const handleSearch = () => {
   clearTimeout(searchTerm.timeout);
   searchTerm.timeout = setTimeout(async () => {
     if (searchTerm.query.length >= 3) {
-      const response =
-        await $fetch(`http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${searchTerm.query}
-`);
+      //http://dataservice.accuweather.com/locations/v1/cities/autocomplete
+      // 47hAIuP8o8M9RSBosP20WsurcfPSgbAu
+      //       const response =
+      //         await $fetch(`http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${searchTerm.query}
+      // `);
+      const response = await $fetch(
+        `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=47hAIuP8o8M9RSBosP20WsurcfPSgbAu&q=${searchTerm.query}`
+      );
       console.log(response);
       // const data = await response.json();
       searchTerm.results = response;
@@ -23,11 +29,19 @@ const handleSearch = () => {
   }, 1000);
 };
 
-const getWeather = async (id) => {
-  const response =
-    await $fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=id:${id}&days=3&aqi=no&alerts=no
-`);
-  console.log(id);
+// const getWeather = async (id) => {
+//   const response =
+//     await $fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=id:${id}&days=3&aqi=no&alerts=no
+// `);
+//   console.log(id);
+//   console.log(response);
+// };
+const getWeather = async (key) => {
+  const response = await $fetch(
+    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${apiKey2}`
+  );
+  // console.log(id);
+  console.log(key);
   console.log(response);
 };
 </script>
@@ -39,7 +53,7 @@ const getWeather = async (id) => {
     {{ searchTerm.query }}
     {{ searchTerm.results }}
     <form>
-      <div class="flex items-center bg-white shadow-lg border rounded-lg">
+      <div class="flex items-center bg-white shadow-lg mx-12 border rounded-lg">
         <i class="p-2 text-indigo-500"><Icon /></i>
         <input
           type="text"
@@ -53,12 +67,14 @@ const getWeather = async (id) => {
 
     <!-- suggestions bar -->
     <div class="bg-amber-400 w-full">
-      <div v-for="result in searchTerm.results" :key="result.id">
+      <div v-for="result in searchTerm.results" :key="result.Key">
         <button
           class="my-2 px-3 w-full hover:font-bold text-left hover:text-indigo-500"
-          @click="getWeather(result.id)"
+          @click="getWeather(result.Key)"
         >
-          {{ result.name }}, {{ result.region }}, {{ result.country }}
+          {{ result.LocalizedName }},
+          {{ result.AdministrativeArea.LocalizedName }},
+          {{ result.Country.LocalizedName }}
         </button>
       </div>
     </div>
